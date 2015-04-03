fadeab50436f1f4f675e52651a7b847b6a44a9ec
damien5314 <damien5314@gmail.com>
2015-04-03 15:00:11 -0700

    Removed unneeded methods from RedditAPI

--- app/src/main/java/com/ddiehl/android/simpleredditreader/web/RedditAPI.java
+++ app/src/main/java/com/ddiehl/android/simpleredditreader/web/RedditAPI.java
@@ -6,25 +6,12 @@ import java.util.List;
 
 import retrofit.Callback;
 import retrofit.client.Response;
-import retrofit.http.Field;
-import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface RedditAPI {
-
-    @FormUrlEncoded @POST("/api/v1/access_token")
-    void getApplicationAuthToken(@Field("grant_type") String grantType,
-                                 @Field("device_id") String deviceId,
-                                 Callback<AuthTokenResponse> callback);
-
-    @FormUrlEncoded @POST("/api/v1/access_token")
-    void getUserAuthToken(@Field("grant_type") String grantType,
-                          @Field("code") String code,
-                          @Field("redirect_uri") String redirectUri,
-                          Callback<AuthTokenResponse> callback);
 
     @GET("/r/{subreddit}/{sort}.json")
     void getLinks(@Path("subreddit") String subreddit,
