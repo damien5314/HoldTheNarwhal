41736ebce422428a80235b03deeea3da4cd5dcf7
damien5314 <damien5314@gmail.com>
2015-04-16 19:47:54 -0700

    Code style fixes

--- app/src/main/java/com/ddiehl/android/simpleredditreader/RedditPreferences.java
+++ app/src/main/java/com/ddiehl/android/simpleredditreader/RedditPreferences.java
@@ -4,28 +4,20 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import com.ddiehl.android.simpleredditreader.events.responses.UserIdentityRetrievedEvent;
-import com.ddiehl.android.simpleredditreader.model.identity.UserIdentity;
-import com.squareup.otto.Subscribe;
-
 import java.util.UUID;
 
 public class RedditPreferences {
     public static final String PREFS_DEVICE_ID = "prefs_device_id";
     public static final String PREF_DEVICE_ID = "pref_device_id";
-
-    // User identity preferences
-
     public static final String COMMENT_SORT = "pref_comment_sort";
     public static final String LINKS_SORT = "pref_links_sort";
     public static final String LINKS_TIMESPAN = "pref_links_timespan";
 
     private static RedditPreferences _instance;
-
-    private Context mContext;
+    private Context mAppContext;
 
     private RedditPreferences(Context context) {
-        mContext = context.getApplicationContext();
+        mAppContext = context.getApplicationContext();
     }
 
     public static RedditPreferences getInstance(Context context) {
@@ -40,7 +32,7 @@ public class RedditPreferences {
     }
 
     public String getDeviceId() {
-        SharedPreferences sp = mContext.getSharedPreferences(PREFS_DEVICE_ID, Context.MODE_PRIVATE);
+        SharedPreferences sp = mAppContext.getSharedPreferences(PREFS_DEVICE_ID, Context.MODE_PRIVATE);
         String deviceId = sp.getString(PREF_DEVICE_ID, null);
         if (deviceId == null) {
             deviceId = generateDeviceId();
@@ -49,51 +41,43 @@ public class RedditPreferences {
     }
 
     private String generateDeviceId() {
-        SharedPreferences sp = mContext.getSharedPreferences(PREFS_DEVICE_ID, Context.MODE_PRIVATE);
+        SharedPreferences sp = mAppContext.getSharedPreferences(PREFS_DEVICE_ID, Context.MODE_PRIVATE);
         String deviceId = UUID.randomUUID().toString();
         sp.edit().putString(PREF_DEVICE_ID, deviceId).apply();
         return deviceId;
     }
 
-    @Subscribe
-    public void saveUserIdentity(UserIdentityRetrievedEvent event) {
-        if (event.isFailed())
-            return;
-
-        UserIdentity identity = event.getUserIdentity();
-    }
-
     public String getCommentSort() {
-        return PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getString(COMMENT_SORT, mContext.getString(R.string.default_comment_sort));
+        return PreferenceManager.getDefaultSharedPreferences(mAppContext)
+                .getString(COMMENT_SORT, mAppContext.getString(R.string.default_comment_sort));
     }
 
     public void saveCommentSort(String pref) {
-        PreferenceManager.getDefaultSharedPreferences(mContext)
+        PreferenceManager.getDefaultSharedPreferences(mAppContext)
                 .edit()
                 .putString(COMMENT_SORT, pref)
                 .apply();
     }
 
     public String getLinksSort() {
-        return PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getString(LINKS_SORT, mContext.getString(R.string.default_links_sort));
+        return PreferenceManager.getDefaultSharedPreferences(mAppContext)
+                .getString(LINKS_SORT, mAppContext.getString(R.string.default_links_sort));
     }
 
     public void saveLinksSort(String pref) {
-        PreferenceManager.getDefaultSharedPreferences(mContext)
+        PreferenceManager.getDefaultSharedPreferences(mAppContext)
                 .edit()
                 .putString(LINKS_SORT, pref)
                 .apply();
     }
 
     public String getLinksTimespan() {
-        return PreferenceManager.getDefaultSharedPreferences(mContext)
-                .getString(LINKS_TIMESPAN, mContext.getString(R.string.default_links_timespan));
+        return PreferenceManager.getDefaultSharedPreferences(mAppContext)
+                .getString(LINKS_TIMESPAN, mAppContext.getString(R.string.default_links_timespan));
     }
 
     public void saveLinksTimespan(String pref) {
-        PreferenceManager.getDefaultSharedPreferences(mContext)
+        PreferenceManager.getDefaultSharedPreferences(mAppContext)
                 .edit()
                 .putString(LINKS_TIMESPAN, pref)
                 .apply();
