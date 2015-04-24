f65f2d41438a046ad62a9c7ffff5a0d6c04f5fd5
damien5314 <damien5314@gmail.com>
2015-04-24 00:05:40 -0700

    Implemented MVP for CommentsFragment

--- app/src/main/java/com/ddiehl/android/simpleredditreader/view/ChooseCommentSortDialog.java
+++ app/src/main/java/com/ddiehl/android/simpleredditreader/view/ChooseCommentSortDialog.java
@@ -14,19 +14,12 @@ import com.ddiehl.android.simpleredditreader.R;
 public class ChooseCommentSortDialog extends DialogFragment {
     private static final String TAG = ChooseCommentSortDialog.class.getSimpleName();
 
-    private static final String ARG_SETTING = "setting";
     public static final String EXTRA_SORT = "com.ddiehl.android.simpleredditreader.extra_sort";
-
-    private String currentSetting;
 
     public ChooseCommentSortDialog() { }
 
-    public static ChooseCommentSortDialog newInstance(String currentSetting) {
-        Bundle args = new Bundle();
-        args.putString(ARG_SETTING, currentSetting);
-        ChooseCommentSortDialog dialog = new ChooseCommentSortDialog();
-        dialog.setArguments(args);
-        return dialog;
+    public static ChooseCommentSortDialog newInstance() {
+        return new ChooseCommentSortDialog();
     }
 
     @NonNull @Override
@@ -35,14 +28,6 @@ public class ChooseCommentSortDialog extends DialogFragment {
 
         // Get selected item if it was passed
         Bundle args = getArguments();
-        currentSetting = args.getString(ARG_SETTING);
-        if (currentSetting != null) {
-            String[] settings = getResources().getStringArray(R.array.comment_sort_options);
-            for (int i = 0; i < settings.length; i++) {
-                if (settings[i].equals(currentSetting))
-                    selectedItem = i;
-            }
-        }
 
         // Build dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
