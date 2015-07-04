2d48c8f839df9ef0b27c4aec8b1d537abc744e08
damien5314 <damien5314@gmail.com>
2015-07-04 15:00:38 -0700

    Removed MVP architecture for settings

--- app/src/main/java/com/ddiehl/android/htn/view/fragments/SettingsFragment.java
+++ app/src/main/java/com/ddiehl/android/htn/view/fragments/SettingsFragment.java
@@ -4,59 +4,106 @@
 
 package com.ddiehl.android.htn.view.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 
 import com.ddiehl.android.htn.BusProvider;
 import com.ddiehl.android.htn.R;
 import com.ddiehl.android.htn.RedditPrefs;
-import com.ddiehl.android.htn.presenter.SettingsPresenter;
-import com.ddiehl.android.htn.presenter.SettingsPresenterImpl;
+import com.ddiehl.android.htn.view.BaseView;
 import com.ddiehl.android.htn.view.MainView;
+import com.flurry.android.FlurryAgent;
 import com.squareup.otto.Bus;
 
-public class SettingsFragment extends PreferenceFragment implements SettingsView {
+import java.util.HashMap;
+import java.util.Map;
+
+public class SettingsFragment extends PreferenceFragment
+        implements BaseView, SharedPreferences.OnSharedPreferenceChangeListener {
 
     private Bus mBus = BusProvider.getInstance();
-    private SettingsPresenter mSettingsPresenter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesName(RedditPrefs.PREFS_USER);
         addPreferencesFromResource(R.xml.preferences);
-        mSettingsPresenter = new SettingsPresenterImpl(getActivity(), this);
-        mSettingsPresenter.updateAllPrefs();
+        updateAllPrefs();
+    }
+
+    public void updateAllPrefs() {
+        Preference root = getPreferenceScreen();
+        updateAllPrefs(root);
+    }
+
+    private void updateAllPrefs(Preference root) {
+        if (root instanceof PreferenceGroup) {
+            PreferenceGroup pGrp = (PreferenceGroup) root;
+            for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                updateAllPrefs(pGrp.getPreference(i));
+            }
+        } else {
+            updatePref(root);
+        }
+    }
+
+    public void updatePref(Preference p) {
+        if (p instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) p;
+            p.setSummary(listPref.getEntry());
+        }
+        if (p instanceof EditTextPreference) {
+            EditTextPreference editTextPref = (EditTextPreference) p;
+            p.setSummary(editTextPref.getText());
+        }
     }
 
     @Override
-    public Preference getRootPreference() {
-        return getPreferenceScreen();
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePref(((Preference) sharedPreferences.getAll().get(key)));
+
+        // Show appreciation for users enabling ads
+        if (key.equals("pref_enable_ads")) {
+            if (sharedPreferences.getBoolean("pref_enable_ads", false)) {
+                showToast(R.string.pref_enable_ads_thanks);
+            }
+        }
+
+        // Send Flurry event
+        Map<String, String> params = new HashMap<>();
+        params.put("key", key);
+        Map prefs = sharedPreferences.getAll();
+        params.put("value", String.valueOf(prefs.get(key)));
+        FlurryAgent.logEvent("setting changed", params);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mBus.register(mSettingsPresenter);
+        mBus.register(this);
     }
 
     @Override
     public void onStop() {
-        mBus.unregister(mSettingsPresenter);
+        mBus.unregister(this);
         super.onStop();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(mSettingsPresenter);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(mSettingsPresenter);
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
