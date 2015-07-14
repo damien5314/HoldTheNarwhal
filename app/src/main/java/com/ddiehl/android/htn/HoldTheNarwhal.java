089c1ba5a00621cb3f2dc8e023f44807c002ce22
damien5314 <damien5314@gmail.com>
2015-07-14 11:26:01 -0700

    Added LeakCanary

--- app/src/main/java/com/ddiehl/android/htn/HoldTheNarwhal.java
+++ app/src/main/java/com/ddiehl/android/htn/HoldTheNarwhal.java
@@ -6,7 +6,13 @@ package com.ddiehl.android.htn;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 
 public class HoldTheNarwhal extends Application {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
 }
