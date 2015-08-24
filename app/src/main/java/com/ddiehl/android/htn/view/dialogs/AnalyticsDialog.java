c57d3fba41ea48d8e88d1f4caa6a107c3388770a
damien5314 <damien5314@gmail.com>
2015-08-24 07:30:52 -0700

    Added dialog for requesting permission to collect analytics

---
+++ app/src/main/java/com/ddiehl/android/htn/view/dialogs/AnalyticsDialog.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015 Damien Diehl. All rights reserved.
+ */
+
+package com.ddiehl.android.htn.view.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+import com.ddiehl.android.htn.R;
+
+public class AnalyticsDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.dialog_analytics_title)
+                .setMessage(R.string.dialog_analytics_message)
+                .setNeutralButton(R.string.dialog_analytics_accept, (dialog, which) -> {
+                    // Set preference to accepted
+                })
+                .setNegativeButton(R.string.dialog_analytics_decline, (dialog, which) -> {
+                    // Set preference to declined
+                })
+                .create();
+    }
+
+}
