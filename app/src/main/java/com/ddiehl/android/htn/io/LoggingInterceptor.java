26121a61431643b3f16d3671626fe09f75c63205
damien5314 <damien5314@gmail.com>
2015-12-11 10:59:36 -0800

    Fixed NullPointerException caused by LoggingInterceptor (maybe)

--- app/src/main/java/com/ddiehl/android/htn/io/LoggingInterceptor.java
+++ app/src/main/java/com/ddiehl/android/htn/io/LoggingInterceptor.java
@@ -1,6 +1,5 @@
 package com.ddiehl.android.htn.io;
 
-import com.ddiehl.android.htn.HoldTheNarwhal;
 import com.ddiehl.android.htn.utils.BaseUtils;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Request;
@@ -11,14 +10,9 @@ import java.io.IOException;
 class LoggingInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
-        try {
-            Request request = chain.request();
-            Response response = chain.proceed(request);
-            if (response != null) BaseUtils.printResponseStatus(response);
-            return response;
-        } catch (Exception e) {
-            HoldTheNarwhal.getLogger().w("Exception occurred while sending HTTP request");
-        }
-        return null;
+        Request request = chain.request();
+        Response response = chain.proceed(request);
+        if (response != null) BaseUtils.printResponseStatus(response);
+        return response;
     }
 }
