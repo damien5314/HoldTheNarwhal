85ce90e3bdac07d20729260fd05e37d9f81a8e86
damien5314 <damien5314@gmail.com>
2015-12-23 16:44:45 -0800

    More work around displaying links in markdown

--- app/src/main/java/com/ddiehl/android/htn/view/widgets/MarkdownTextView.java
+++ app/src/main/java/com/ddiehl/android/htn/view/widgets/MarkdownTextView.java
@@ -9,11 +9,6 @@ import android.widget.TextView;
 
 import com.ddiehl.android.htn.R;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import in.uncod.android.bypass.Bypass;
@@ -49,100 +44,29 @@ public class MarkdownTextView extends TextView {
     if (isInEditMode()) {
       super.setText(text, type);
     } else {
-      text = linkify(text);
       Bypass b = BypassWrapper.getInstance(getContext());
       CharSequence formatted = b.markdownToSpannable(text.toString());
       SpannableString s = SpannableString.valueOf(formatted);
+
+      // Add links for /r/ and /u/ patterns
       Linkify.addLinks(s, Pattern.compile("\\s/*[ru](ser)*/[^ \n]*"), "https://www.reddit.com", null,
           (match, url) -> {
             url = url.trim();
             if (!url.startsWith("/")) url = "/" + url;
             return url;
           });
+
+      // Add links missing protocol
+      Linkify.addLinks(s, Pattern.compile("\\swww\\.[^ \\n]*"), "http://", null,
+          (match, url) -> {
+            return url.trim();
+          });
+
+      // Add links with any protocol
+      Linkify.addLinks(s, Pattern.compile("[a-z]+://[^ \\n]*"), null);
+
       super.setText(s, type);
     }
-  }
-
-  private CharSequence linkify(CharSequence markdown) {
-//    markdown = linkifySubreddit(markdown, "\\s\\/*r\\/[^ \n]*");
-    markdown = linkifyUrl(markdown, "[a-z]+://[^ \\n]*");
-    return markdown;
-  }
-
-  private CharSequence linkifySubreddit(CharSequence markdown, String regex) {
-    Pattern p = Pattern.compile(regex);
-    Matcher m = p.matcher(markdown);
-    List<Integer> bounds = new ArrayList<>();
-    while (m.find()) {
-      int start = m.start(); // Inclusive
-      int end = m.end(); // Exclusive
-      // Remove the white space
-      while (markdown.charAt(start) != '/' && markdown.charAt(start) != 'r') start++;
-      bounds.add(start);
-      bounds.add(end);
-    }
-    // Insert links from saved bounds
-    StringBuilder result = new StringBuilder(markdown);
-    for (int i = bounds.size() - 2; i >= 0; i -= 2) {
-      String s = result.substring(bounds.get(i), bounds.get(i+1));
-//      result.insert(bounds.get(i+1), "](https://www.reddit.com" + s + ")");
-//      result.insert(bounds.get(i), "[");
-//      if (s.charAt(0) != '/') result.insert(bounds.get(i), "https://www.reddit.com/");
-//      else result.insert(bounds.get(i), "https://www.reddit.com");
-//      result.insert(bounds.get(i+1), "</a>");
-//      result.insert(bounds.get(i), "<a href='https://www.reddit.com" + s + "'>");
-
-    }
-    return result;
-  }
-
-  private CharSequence linkifyUrl(CharSequence markdown, String regex) {
-    Pattern p = Pattern.compile(regex);
-    Matcher m = p.matcher(markdown);
-    List<Integer> bounds = new ArrayList<>();
-    while (m.find()) {
-      int start = m.start(); // Inclusive
-      int end = m.end(); // Exclusive
-      // Check for a URL wrapped in parentheses, where the last parentheses was included
-      if (start > 0 && markdown.charAt(start-1) == '(' && markdown.charAt(end-1) == ')') {
-        end--; // Get rid of closing parentheses
-      }
-      if ((start == 0 || markdown.charAt(start-1) != '<')
-//          && (end < markdown.length() - 1 && markdown.charAt(end-1) != '>')
-          ) {
-        bounds.add(start);
-        bounds.add(end);
-      }
-    }
-    // Insert brackets from saved locations
-    StringBuilder result = new StringBuilder(markdown);
-    for (int i = bounds.size() - 2; i >= 0; i -= 2) {
-      result.insert(bounds.get(i+1), ">");
-      result.insert(bounds.get(i), "<");
-    }
-    return result;
-  }
-
-  // http://stackoverflow.com/questions/285619/how-to-detect-the-presence-of-url-in-a-string
-  private String surroundUrlWithBrackets(String markdown) {
-    String [] parts = markdown.split("\\s+");
-    for(int i = 0; i < parts.length; i++) {
-      if (isURL(parts[i])) {
-        parts[i] = "<" + parts[i] + ">";
-      }
-    }
-    StringBuilder b = new StringBuilder();
-    for (String s : parts) b.append(s).append(" ");
-    markdown = b.toString();
-    return markdown;
-  }
-
-  private boolean isURL(String s) {
-    try {
-      new URL(s);
-      return true;
-    } catch (MalformedURLException ignored) { }
-    return false;
   }
 
   private static class BypassWrapper {
