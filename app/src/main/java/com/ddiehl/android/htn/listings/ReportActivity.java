6cc8efd7e094555af2a3fb94af800b5f7046b8de
Damien Diehl <damien5314@gmail.com>
2016-11-18 09:00:06 -0800

    Make API call for report form and pass options into ReportDialog

--- app/src/main/java/com/ddiehl/android/htn/listings/ReportActivity.java
+++ app/src/main/java/com/ddiehl/android/htn/listings/ReportActivity.java
@@ -1,25 +1,45 @@
 package com.ddiehl.android.htn.listings;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 import com.ddiehl.android.htn.HoldTheNarwhal;
 import com.ddiehl.android.htn.view.TransparentBaseActivity;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 import rxreddit.api.RedditService;
+import rxreddit.model.ReportForm;
+import rxreddit.model.SubredditRules;
 
 
 /**
  * Invisible Activity for requesting data from API for reporting a listing,
  * then displaying a dialog with options.
  */
-public class ReportActivity extends TransparentBaseActivity {
+public class ReportActivity extends TransparentBaseActivity
+        implements ReportDialog.Listener {
 
     public static final String EXTRA_LISTING_FULLNAME = "EXTRA_LISTING_FULLNAME";
+
+    public static final int RESULT_GET_REPORT_FORM_ERROR = 10;
+    public static final int RESULT_REPORT_ERROR = 11;
+    public static final int RESULT_REPORT_SUCCESS = Activity.RESULT_OK;
+
+    static final String[] SITE_RULES = {
+            "Spam",
+            "Personal and confidential information",
+            "Threatening, harrassing, or inciting violence"
+    };
 
     public static Intent getIntent(Context context, String fullname) {
         Intent intent = new Intent(context, ReportActivity.class);
@@ -43,6 +63,59 @@ public class ReportActivity extends TransparentBaseActivity {
     }
 
     void getReportDetails() {
-        showSpinner();
+        mRedditService.getReportForm(getListingFullname())
+                .doOnSubscribe(this::showSpinner)
+                .doOnUnsubscribe(this::dismissSpinner)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onReportFormRetrieved(), onGetReportFormError());
     }
+
+    Action1<Throwable> onGetReportFormError() {
+        return (error) -> {
+            setResult(RESULT_GET_REPORT_FORM_ERROR);
+            finish();
+        };
+    }
+
+    Action1<ReportForm> onReportFormRetrieved() {
+        return (form) -> {
+            List<SubredditRules.Rule> rules = form.getRules();
+
+            // Get short names for each rule
+            String[] ruleNames = new String[rules.size()];
+            for (int i = 0; i < rules.size(); i++) {
+                ruleNames[i] = rules.get(i).getShortName();
+            }
+
+            // Show dialog
+            showReportDialogWithRules(ruleNames, SITE_RULES);
+        };
+    }
+
+    void showReportDialogWithRules(String[] subredditRules, String[] siteRules) {
+        ReportDialog dialog = new ReportDialogBuilder(subredditRules, siteRules)
+                .build();
+
+        dialog.show(getSupportFragmentManager(), ReportDialog.TAG);
+    }
+
+    //region ReportDialog.Listener
+
+    @Override
+    public void onRuleSubmitted(String rule) {
+        Toast.makeText(this, rule, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSiteRuleSubmitted(String rule) {
+        Toast.makeText(this, "Site rule: " + rule, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onOtherSubmitted(String reason) {
+        Toast.makeText(this, "Other: " + reason, Toast.LENGTH_LONG).show();
+    }
+
+    //endregion
 }
--- app/src/main/java/com/ddiehl/android/htn/listings/ReportDialog.java
+++ app/src/main/java/com/ddiehl/android/htn/listings/ReportDialog.java
@@ -1,31 +1,65 @@
 package com.ddiehl.android.htn.listings;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
 import com.ddiehl.android.htn.R;
+import com.hannesdorfmann.fragmentargs.annotation.Arg;
+import com.hannesdorfmann.fragmentargs.annotation.FragmentWithArgs;
 
-import static android.app.Activity.RESULT_CANCELED;
-import static android.app.Activity.RESULT_OK;
 
-
+@FragmentWithArgs
 public class ReportDialog extends DialogFragment {
 
-    public static final String EXTRA_OPTION = "EXTRA_OPTION";
+    public interface Listener {
+        void onRuleSubmitted(String rule);
+        void onSiteRuleSubmitted(String rule);
+        void onOtherSubmitted(String reason);
+    }
 
-    String[] mReportOptions;
-    String[] mReportOptionValues;
+    public static final String TAG = ReportDialog.class.getSimpleName();
+    public static final String EXTRA_RULE = "EXTRA_RULE";
+    public static final String EXTRA_SITE_RULE = "EXTRA_SITE_RULE";
+    public static final String EXTRA_OTHER = "EXTRA_OTHER";
+
+    @Arg String[] mRules;
+    @Arg String[] mSiteRules;
+
+    Listener mListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (!(context instanceof Listener)) {
+            throw new RuntimeException("Context must implement ReportDialog.Listener");
+        }
+        mListener = (Listener) context;
+    }
+
+    @Override
+    public void onDetach() {
+        mListener = null;
+        super.onDetach();
+    }
 
     @NonNull @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Concatenate all options into single array
+        String[] reportOptions = new String[mRules.length + mSiteRules.length];
+        System.arraycopy(mRules, 0, reportOptions, 0, mRules.length);
+        System.arraycopy(mSiteRules, 0, reportOptions, mRules.length, mSiteRules.length);
+
+        // Create dialog with options
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.report_menu_title)
-                .setSingleChoiceItems(mReportOptions, -1, null)
+                .setSingleChoiceItems(reportOptions, -1, null)
                 .setPositiveButton(R.string.report_submit, onSubmit())
                 .setNegativeButton(R.string.report_cancel, onCancel())
                 .create();
@@ -33,28 +67,37 @@ public class ReportDialog extends DialogFragment {
 
     DialogInterface.OnClickListener onSubmit() {
         return (dialogInterface, which) -> {
-            String option = mReportOptionValues[which];
-            finish(option);
+            if (which < mRules.length) {
+                submit(mRules[which], null, null);
+            } else if (which < mRules.length + mSiteRules.length) {
+                submit(null, mSiteRules[which - mRules.length], null);
+            } else {
+                // TODO Support "other" reports
+                submit(null, null, "");
+            }
         };
     }
 
     DialogInterface.OnClickListener onCancel() {
-        return (dialogInterface, which) -> finish(RESULT_CANCELED, null);
+        return (dialogInterface, which) -> dismiss();
     }
 
-    private void finish(@NonNull String sort) {
+    private void submit(@Nullable String rule, @Nullable String siteRule, @Nullable String other) {
         Intent data = new Intent();
-        data.putExtra(EXTRA_OPTION, sort);
-        finish(RESULT_OK, data);
-    }
 
-    private void finish(int resultCode, Intent data) {
-        if (getTargetFragment() != null) {
-            getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, data);
+        if (rule != null) {
+            mListener.onRuleSubmitted(rule);
             dismiss();
-        } else {
-            getActivity().setResult(resultCode);
-            getActivity().finish();
+        }
+
+        if (siteRule != null) {
+            mListener.onSiteRuleSubmitted(siteRule);
+            dismiss();
+        }
+
+        if (other != null) {
+            mListener.onOtherSubmitted(other);
+            dismiss();
         }
     }
 }
