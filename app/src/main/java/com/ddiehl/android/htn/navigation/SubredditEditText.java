30242c6b75aa958bf088934c9a28ec4df8c85259
Damien Diehl <damien5314@gmail.com>
2016-11-28 22:22:24 -0800

    StackOverflow fix in SubredditEditText (#22)
    * Fix for StackOverflowError in SubredditEditText
    
    * Added comments
    
    * Refactoring

--- app/src/main/java/com/ddiehl/android/htn/navigation/SubredditEditText.java
+++ app/src/main/java/com/ddiehl/android/htn/navigation/SubredditEditText.java
@@ -3,6 +3,7 @@ package com.ddiehl.android.htn.navigation;
 import android.content.Context;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.View;
@@ -13,51 +14,79 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.OnEditorAction;
 import butterknife.OnFocusChange;
-import butterknife.OnTextChanged;
 
 public class SubredditEditText extends AppCompatEditText {
 
+    TextWatcher mTextChangedListener;
+
     public SubredditEditText(Context context) {
         this(context, null);
+        init();
     }
 
     public SubredditEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
         ButterKnife.bind(this);
+        init();
     }
 
-    private CharSequence before;
-
-    @OnTextChanged(callback = OnTextChanged.Callback.BEFORE_TEXT_CHANGED)
-    void beforeTextChanged(CharSequence input, int start, int count, int after) {
-        before = input.subSequence(0, input.length());
+    void init() {
+        mTextChangedListener = getTextChangedListener();
+        addTextChangedListener(mTextChangedListener);
     }
 
-    @OnTextChanged(callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
-    void afterTextChanged(Editable input) {
-        if (input.length() == 0) { // Blank field
-            return;
-        }
+    TextWatcher getTextChangedListener() {
+        return new TextWatcher() {
+            private CharSequence before;
 
-        if (input.toString().equals(" ")) {
-            input.clear();
-            return;
-        }
+            @Override
+            public void beforeTextChanged(CharSequence input, int start, int count, int after) {
+                before = input.subSequence(0, input.length());
+            }
 
-        if (!input.toString().startsWith("/r/")) {
-            input.insert(0, "/r/");
-            return;
-        }
+            @Override
+            public void onTextChanged(CharSequence input, int start, int before, int count) {
+            }
 
-        if (input.length() == 3) {
-            input.clear();
-            return;
-        }
+            @Override
+            public void afterTextChanged(Editable input) {
+                // Blank field
+                if (input.length() == 0) {
+                    return;
+                }
 
-        CharSequence cs = input.subSequence(0, 3);
-        if (!cs.toString().equals("/r/") || input.toString().contains(" ")) {
-            input.replace(0, input.length(), before);
-        }
+                // Trim single spaces
+                if (input.toString().equals(" ")) {
+                    removeTextChangedListener(mTextChangedListener);
+                    input.clear();
+                    addTextChangedListener(mTextChangedListener);
+                    return;
+                }
+
+                // If input doesn't start with the subreddit link prefix, add it
+                if (!input.toString().startsWith("/r/")) {
+                    removeTextChangedListener(mTextChangedListener);
+                    input.insert(0, "/r/");
+                    addTextChangedListener(mTextChangedListener);
+                    return;
+                }
+
+                // If input is length 3 ("/r/"), just clear the field
+                if (input.length() == 3) {
+                    removeTextChangedListener(mTextChangedListener);
+                    input.clear();
+                    addTextChangedListener(mTextChangedListener);
+                    return;
+                }
+
+                // Extra check if user typed a space
+                if (!input.subSequence(0, 3).equals("/r/") || input.toString().contains(" ")) {
+                    removeTextChangedListener(mTextChangedListener);
+                    input.replace(0, input.length(), before);
+                    addTextChangedListener(mTextChangedListener);
+                }
+            }
+        };
     }
 
     @OnFocusChange
