c306a775febb94bc0f6d71d0e34f414d25ced980
Damien Diehl <damien5314@gmail.com>
2016-12-14 20:57:04 -0800

    Linkifying subreddit and user page links, and mitigating the emphasis formatting issues with special parsing

--- app/src/main/java/com/ddiehl/android/htn/view/MarkdownTextView.java
+++ app/src/main/java/com/ddiehl/android/htn/view/MarkdownTextView.java
@@ -3,12 +3,17 @@ package com.ddiehl.android.htn.view;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
-import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.method.LinkMovementMethod;
+import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
 import android.util.AttributeSet;
 
 import com.ddiehl.android.htn.HoldTheNarwhal;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.inject.Inject;
@@ -53,31 +58,77 @@ public class MarkdownTextView extends AppCompatTextView {
         if (isInEditMode() || mBypass == null) {
             super.setText(text, type);
         } else {
-            CharSequence formatted = mBypass.markdownToSpannable(text.toString());
-            SpannableString s = SpannableString.valueOf(formatted);
+            Pattern redditLinkMatcher = Pattern.compile("(?:^)/?[ru]/\\S+", Pattern.MULTILINE);
+            Pattern missingProtocolMatcher = Pattern.compile("(http|https?://)?www\\.[^ \\n]*");
+            Pattern anyProtocolMatcher = Pattern.compile("[a-z]+://[^ \\n]*");
+
+            // Find links in the text and surround them with autolink tags '<>' before processing
+//            autolink(sb, redditLinkMatcher);
+//            autolink(sb, missingProtocolMatcher);
+//            autolink(sb, anyProtocolMatcher);
+
+            CharSequence markdown = mBypass.markdownToSpannable(text.toString());
+            SpannableStringBuilder formatted = new SpannableStringBuilder(markdown);
 
             // Add links for /r/ and /u/ patterns
             Linkify.addLinks(
-                    s, Pattern.compile("\\s/*[ru](ser)*/[^ \n]*"), "https://www.reddit.com", null,
+                    formatted, redditLinkMatcher, "https://www.reddit.com", null,
                     (match, url) -> {
                         url = url.trim();
-                        if (!url.startsWith("/")) url = "/" + url;
+                        if (!url.startsWith("/")) {
+                            url = "/" + url;
+                        }
                         return url;
                     }
             );
 
             // Add links missing protocol
-            Linkify.addLinks(
-                    s, Pattern.compile("\\swww\\.[^ \\n]*"), "http://", null,
-                    (match, url) -> {
-                        return url.trim();
-                    }
-            );
+//            Linkify.addLinks(
+//                    s, missingProtocolMatcher, "http://", null,
+//                    (match, url) -> {
+//                        return url.trim();
+//                    }
+//            );
 
             // Add links with any protocol
-            Linkify.addLinks(s, Pattern.compile("[a-z]+://[^ \\n]*"), null);
+//            Linkify.addLinks(s, anyProtocolMatcher, null);
 
-            super.setText(s, type);
+            convertFormattingWithinLinks(formatted);
+
+            super.setText(formatted, type);
+        }
+    }
+
+    void convertFormattingWithinLinks(SpannableStringBuilder string) {
+        URLSpan[] spans = string.getSpans(0, string.length(), URLSpan.class);
+
+        for (URLSpan span : spans) {
+            int start = string.getSpanStart(span);
+            int end = string.getSpanEnd(span);
+
+            StyleSpan[] innerSpans = string.getSpans(start, end, StyleSpan.class);
+            for (StyleSpan innerSpan : innerSpans) {
+                int spanStart = string.getSpanStart(innerSpan);
+                int spanEnd = string.getSpanEnd(innerSpan);
+                string.insert(spanStart, "_");
+                string.insert(spanEnd + 1, "_");
+                string.removeSpan(innerSpan);
+            }
+        }
+    }
+
+    void autolink(StringBuilder builder, Pattern pattern) {
+        List<Integer> matchIndices = new ArrayList<>();
+        Matcher matcher = pattern.matcher(builder);
+        while (matcher.find()) {
+            matchIndices.add(matcher.start());
+            matchIndices.add(matcher.end());
+        }
+
+        int i = matchIndices.size() - 1;
+        while (i > 0) {
+            builder.insert(matchIndices.get(i--), ">");
+            builder.insert(matchIndices.get(i--), "<");
         }
     }
 }
