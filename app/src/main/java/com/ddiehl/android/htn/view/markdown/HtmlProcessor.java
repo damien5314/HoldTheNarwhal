5be1ab025699342e9efc11bd4184780d6c13735d
Damien Diehl <damien5314@gmail.com>
2017-01-17 10:05:20 -0800

    Fix URLSpans for reddit links

--- app/src/main/java/com/ddiehl/android/htn/view/markdown/HtmlProcessor.java
+++ app/src/main/java/com/ddiehl/android/htn/view/markdown/HtmlProcessor.java
@@ -3,10 +3,13 @@ package com.ddiehl.android.htn.view.markdown;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.text.Html;
+import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.style.URLSpan;
 
 import com.ddiehl.android.htn.utils.AndroidUtils;
+import com.ddiehl.android.htn.view.URLSpanNoUnderline;
 
 import static android.text.Html.FROM_HTML_MODE_LEGACY;
 
@@ -30,6 +33,8 @@ public class HtmlProcessor {
         // Strip any trailing new lines
         trimTrailingNewLines(formatted);
 
+        fixUrlSpansForRedditLinks(formatted);
+
         // Convert URLSpans to no underline form
         AndroidUtils.convertUrlSpansToNoUnderlineForm(formatted);
 
@@ -39,6 +44,27 @@ public class HtmlProcessor {
     void trimTrailingNewLines(@NonNull SpannableStringBuilder text) {
         while ("\n".equals(text.subSequence(text.length() - 1, text.length()).toString())) {
             text.delete(text.length() - 1, text.length());
+        }
+    }
+
+    void fixUrlSpansForRedditLinks(@NonNull SpannableStringBuilder text) {
+        // Get all URLSpans
+        URLSpan[] spans = text.getSpans(0, text.length(), URLSpan.class);
+
+        for (URLSpan span : spans) {
+            String url = span.getURL();
+
+            // Detect if the span is for a reddit link
+            if (url.startsWith("/r/") || url.startsWith("/u/")) {
+                // Cache start and end markers
+                int start = text.getSpanStart(span);
+                int end = text.getSpanEnd(span);
+                // Prepend the hostname to the linked one
+                String newUrl = "https://www.reddit.com" + url;
+                // Remove old span and set new one
+                text.removeSpan(span);
+                text.setSpan(new URLSpanNoUnderline(newUrl), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
         }
     }
 }
