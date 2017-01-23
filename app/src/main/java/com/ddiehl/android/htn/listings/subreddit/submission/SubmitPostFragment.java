2fde7886787c3ad8b23fb60b5c7f7b625a3a62b4
Damien Diehl <damien5314@gmail.com>
2017-01-23 08:26:47 -0800

    View bindings for layout

--- app/src/main/java/com/ddiehl/android/htn/listings/subreddit/submission/SubmitPostFragment.java
+++ app/src/main/java/com/ddiehl/android/htn/listings/subreddit/submission/SubmitPostFragment.java
@@ -2,7 +2,12 @@ package com.ddiehl.android.htn.listings.subreddit.submission;
 
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.ddiehl.android.htn.HoldTheNarwhal;
 import com.ddiehl.android.htn.R;
@@ -19,6 +24,20 @@ public class SubmitPostFragment extends BaseFragment {
 
     @BindView(R.id.coordinator_layout)
     CoordinatorLayout mCoordinatorLayout;
+    @BindView(R.id.submission_type_tabs)
+    TabLayout mTabLayout;
+    @BindView(R.id.submission_url)
+    EditText mUrlEditText;
+    @BindView(R.id.submission_image)
+    ImageView mImageView;
+    @BindView(R.id.submission_title)
+    EditText mTitleEditText;
+    @BindView(R.id.submission_text)
+    EditText mTextEditText;
+    @BindView(R.id.send_replies_to_inbox)
+    CheckBox mSendRepliesToInboxCheckbox;
+    @BindView(R.id.submission_submit)
+    Button mSubmitButton;
 
     @Override protected int getLayoutResId() {
         return R.layout.submission_fragment;
