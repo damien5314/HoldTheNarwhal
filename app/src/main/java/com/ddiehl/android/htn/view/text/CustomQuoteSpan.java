41e9764b472ef01fc35e1206dd951a85cbc363da
Damien Diehl <damien5314@gmail.com>
2017-06-17 15:57:26 -0700

    Fix getSpanTypeId crash (#62)
    * CustomQuoteSpan should extend from QuoteSpan instead of copying it
    
    * CustomBulletSpan extends from BulletSpan instead of copying it

--- app/src/main/java/com/ddiehl/android/htn/view/text/CustomQuoteSpan.java
+++ app/src/main/java/com/ddiehl/android/htn/view/text/CustomQuoteSpan.java
@@ -1,81 +1,30 @@
-/*
- * Copyright (C) 2006 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.ddiehl.android.htn.view.text;
 
-import android.annotation.SuppressLint;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.os.Parcel;
 import android.support.annotation.ColorInt;
 import android.text.Layout;
-import android.text.ParcelableSpan;
-import android.text.style.LeadingMarginSpan;
+import android.text.style.QuoteSpan;
 
-@SuppressLint("ParcelCreator")
-public class CustomQuoteSpan implements LeadingMarginSpan, ParcelableSpan {
+/**
+ * A {@link QuoteSpan} implementation that overrides {@link QuoteSpan#STRIPE_WIDTH}
+ * and {@link QuoteSpan#GAP_WIDTH}.
+ */
+public class CustomQuoteSpan extends QuoteSpan {
 
     private static final int STRIPE_WIDTH = 4;
     private static final int GAP_WIDTH = 16;
 
-    private final int mColor;
-
-    public CustomQuoteSpan() {
-        super();
-        mColor = 0xff0000ff;
-    }
-
     public CustomQuoteSpan(@ColorInt int color) {
-        super();
-        mColor = color;
+        super(color);
     }
 
-    public CustomQuoteSpan(Parcel src) {
-        mColor = src.readInt();
-    }
-
-    public int getSpanTypeId() {
-        return getSpanTypeIdInternal();
-    }
-
-    public int getSpanTypeIdInternal() {
-        return 9; // Value of TextUtils.QUOTE_SPAN (hidden API)
-    }
-
-    public int describeContents() {
-        return 0;
-    }
-
-    public void writeToParcel(Parcel dest, int flags) {
-        writeToParcelInternal(dest, flags);
-    }
-
-    public void writeToParcelInternal(Parcel dest, int flags) {
-        dest.writeInt(mColor);
-    }
-
-    @ColorInt
-    public int getColor() {
-        return mColor;
-    }
-
+    @Override
     public int getLeadingMargin(boolean first) {
         return STRIPE_WIDTH + GAP_WIDTH;
     }
 
+    @Override
     public void drawLeadingMargin(
             Canvas c, Paint p, int x, int dir,
             int top, int baseline, int bottom,
@@ -85,7 +34,7 @@ public class CustomQuoteSpan implements LeadingMarginSpan, ParcelableSpan {
         int color = p.getColor();
 
         p.setStyle(Paint.Style.FILL);
-        p.setColor(mColor);
+        p.setColor(getColor());
 
         c.drawRect(x, top, x + dir * STRIPE_WIDTH, bottom, p);
 
