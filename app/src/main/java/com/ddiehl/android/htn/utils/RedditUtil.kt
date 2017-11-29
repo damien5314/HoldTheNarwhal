d5f3fbf355f8407a2f725a7be6aafc2ba7132bf9
Damien Diehl <damien5314@gmail.com>
2017-11-28 22:22:57 -0800

    fix

--- app/src/main/java/com/ddiehl/android/htn/utils/RedditUtil.kt
+++ app/src/main/java/com/ddiehl/android/htn/utils/RedditUtil.kt
@@ -7,7 +7,7 @@ import rxreddit.model.Listing
 /**
  * Flattens list of comments, marking each comment with depth
  */
-fun flattenCommentList(commentList: MutableList<Listing>) {
+fun flattenCommentList2(commentList: MutableList<Listing>) {
     for (i in 0 until commentList.size) {
         val listing = commentList[i]
         if (listing is Comment) {
@@ -26,5 +26,33 @@ fun flattenCommentList(commentList: MutableList<Listing>) {
             val moreComments = listing as CommentStub
             moreComments.depth = moreComments.depth + 1 // Increase depth by 1
         }
+    }
+}
+
+
+/**
+ * Flattens list of comments, marking each comment with depth
+ */
+fun flattenCommentList(commentList: MutableList<Listing>) {
+    var i = 0
+    while (i < commentList.size) {
+        val listing = commentList[i]
+        if (listing is Comment) {
+            val repliesListing = listing.replies
+            if (repliesListing != null) {
+                val replies = repliesListing.data.children
+                flattenCommentList(replies)
+            }
+            listing.depth = listing.depth + 1 // Increase depth by 1
+            if (listing.replies != null) {
+                // Add all of the replies to commentList
+                commentList.addAll(i + 1, listing.replies.data.children)
+                listing.replies = null // Remove replies for comment
+            }
+        } else { // Listing is a CommentStub
+            val moreComments = listing as CommentStub
+            moreComments.depth = moreComments.depth + 1 // Increase depth by 1
+        }
+        i++
     }
 }
