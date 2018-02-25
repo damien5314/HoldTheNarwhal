5838645718f2b860b698a102c6c747894ed9ebcd
Damien Diehl <damien5314@gmail.com>
2018-02-24 22:39:17 -0800

    Remove dead code

--- app/src/main/java/com/ddiehl/android/htn/di/SharedModule.java
+++ app/src/main/java/com/ddiehl/android/htn/di/SharedModule.java
@@ -18,18 +18,4 @@ public class SharedModule {
     Context providesContext() {
         return mContext;
     }
-
-//    @Singleton @Provides
-//    @Nullable Bypass providesBypass(Context context) {
-//        try {
-//            Bypass.Options options = new Bypass.Options();
-//            options.setBlockQuoteColor(
-//                    ContextCompat.getColor(context, R.color.markdown_quote_block)
-//            );
-//            return new Bypass(context, options);
-//        } catch (UnsatisfiedLinkError error) {
-//            Timber.w("Bypass is unavailable");
-//            return null;
-//        }
-//    }
 }
