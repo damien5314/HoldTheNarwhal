be2a1a35c883027ee64890bc3e13eeae6dce13f9
Damien Diehl <damien5314@gmail.com>
2018-03-03 23:41:02 -0800

    Mark unread messages read when navigating to inbox (#83)

--- app/src/main/java/com/ddiehl/android/htn/listings/inbox/InboxPresenter.java
+++ app/src/main/java/com/ddiehl/android/htn/listings/inbox/InboxPresenter.java
@@ -6,10 +6,13 @@ import com.ddiehl.android.htn.navigation.RedditNavigationView;
 import com.ddiehl.android.htn.view.MainView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import rxreddit.model.Listing;
+import rxreddit.model.ListingResponse;
 import rxreddit.model.PrivateMessage;
 import timber.log.Timber;
 
@@ -39,7 +42,7 @@ public class InboxPresenter extends BaseListingsPresenter {
         String prevId = append ? null : mPrevPageListingId;
         String nextId = append ? mNextPageListingId : null;
 
-        mRedditService.getInbox(show, prevId, nextId)
+        mRedditService.getInbox(show, true, prevId, nextId)
                 .flatMap(this::checkNullResponse)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -52,7 +55,10 @@ public class InboxPresenter extends BaseListingsPresenter {
                     mNextRequested = false;
                 })
                 .subscribe(
-                        listings -> onListingsLoaded(listings, append),
+                        listings -> {
+                            onListingsLoaded(listings, append);
+                            markMessagesRead(listings);
+                        },
                         error -> {
                             if (error instanceof IOException) {
                                 String message = mContext.getString(R.string.error_network_unavailable);
@@ -98,5 +104,31 @@ public class InboxPresenter extends BaseListingsPresenter {
 
     public void onViewSelected(String show) {
         refreshData();
+    }
+
+    private void markMessagesRead(ListingResponse listings) {
+        final List<String> messageFullnames = new ArrayList<>();
+        for (Listing listing : listings.getData().getChildren()) {
+            if (listing instanceof PrivateMessage
+                    && ((PrivateMessage) listing).isUnread()) {
+                messageFullnames.add(listing.getFullName());
+            }
+        }
+        if (!messageFullnames.isEmpty()) {
+            final String commaSeparated = getCommaSeparatedString(messageFullnames);
+            mRedditService.markMessagesRead(commaSeparated)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(() -> { }, Timber::e);
+        }
+    }
+
+    private String getCommaSeparatedString(List<String> strings) {
+        final StringBuilder result = new StringBuilder();
+        for (String string : strings) {
+            result.append(string).append(",");
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
     }
 }
