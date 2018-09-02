a89f9231febf8046855e9c2c00cbd77f980e11e5
Damien Diehl <damien5314@gmail.com>
2018-09-01 17:39:57 -0700

    Move logged out check for unread inbox messages (#94)

--- app/src/main/java/com/ddiehl/android/htn/notifications/UnreadInboxCheckJobService.kt
+++ app/src/main/java/com/ddiehl/android/htn/notifications/UnreadInboxCheckJobService.kt
@@ -61,6 +61,10 @@ class UnreadInboxCheckJobService : JobService() {
     }
 
     private fun checkUnreads(params: JobParameters?) {
+        if (!redditService.isUserAuthorized) {
+            jobFinished(params, false)
+            return
+        }
         val unreadInboxChecker = UnreadInboxChecker(redditService)
         subscription = unreadInboxChecker.check()
                 .subscribeOn(Schedulers.io())
@@ -88,6 +92,6 @@ class UnreadInboxCheckJobService : JobService() {
     }
 
     private fun onInboxFetchError(throwable: Throwable) {
-        Timber.w(throwable)
+        Timber.e(throwable)
     }
 }
