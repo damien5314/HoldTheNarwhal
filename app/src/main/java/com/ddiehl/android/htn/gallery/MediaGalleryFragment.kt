aee6d28daed39349cc4ffce551c4726bb5dddf67
Damien Diehl <damien5314@gmail.com>
2022-07-09 13:20:40 -0400

    suppress ViewPager scrolling when we zooming in

--- app/src/main/java/com/ddiehl/android/htn/gallery/MediaGalleryFragment.kt
+++ app/src/main/java/com/ddiehl/android/htn/gallery/MediaGalleryFragment.kt
@@ -2,9 +2,7 @@ package com.ddiehl.android.htn.gallery
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -16,6 +14,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.ortiz.touchview.TouchImageView
 import rxreddit.model.GalleryItem
 import rxreddit.model.Link
+import timber.log.Timber
 
 /**
  * Fragment for displaying a UI to browse media in a gallery listing.
@@ -55,13 +54,35 @@ class MediaGalleryFragment : DialogFragment() {
     private val viewPager by lazy { requireView().findViewById<ViewPager2>(R.id.media_gallery_view_pager) }
     private val viewPagerTabs by lazy { requireView().findViewById<TabLayout>(R.id.media_gallery_view_pager_tabs) }
     private var tabLayoutMediator: TabLayoutMediator? = null
+    private lateinit var scaleGestureDetector: ScaleGestureDetector
 
     init {
         HoldTheNarwhal.getApplicationComponent().inject(this)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        Dialog(requireContext(), R.style.DialogOverlay)
+        Dialog(requireContext(), R.style.DialogOverlay).also {
+            // TODO: Delete overloads you don't need
+            this.scaleGestureDetector = ScaleGestureDetector(
+                requireContext(),
+                object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                    override fun onScale(detector: ScaleGestureDetector?): Boolean {
+                        return super.onScale(detector)
+                    }
+
+                    override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+                        Timber.d("[dcd] LOCKING user input for swiping")
+                        viewPager.isUserInputEnabled = false
+                        return false
+                    }
+
+                    override fun onScaleEnd(detector: ScaleGestureDetector?) {
+                        Timber.d("[dcd] UNLOCKING user input for swiping")
+                        super.onScaleEnd(detector)
+                        viewPager.isUserInputEnabled = true
+                    }
+                })
+        }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.media_gallery_fragment, container, false)
@@ -80,6 +101,19 @@ class MediaGalleryFragment : DialogFragment() {
             tab.text = (position + 1).toString()
         }
             .also { it.attach() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewPager.setOnTouchListener { _, event ->
+            this.scaleGestureDetector?.onTouchEvent(event) ?: false
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // TODO: Remove gesture detector
+        viewPager.setOnTouchListener(null)
     }
 
     private class MediaGalleryViewPagerAdapter(
@@ -107,6 +141,38 @@ class MediaGalleryFragment : DialogFragment() {
     private class MediaGalleryItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val itemImageView: TouchImageView = itemView.findViewById(R.id.media_gallery_item_image)
+
+        init {
+            suppressParentScrollingDuringZoom()
+        }
+
+        // Adapted from https://github.com/MikeOrtiz/TouchImageView/issues/191
+        private fun suppressParentScrollingDuringZoom() {
+            itemImageView.setOnTouchListener { view, event ->
+                //can scroll horizontally checks if there's still a part of the image
+                //that can be scrolled until you reach the edge
+                return@setOnTouchListener if (event.pointerCount >= 2 ||
+                    view.canScrollHorizontally(1) && view.canScrollHorizontally(-1)
+                ) {
+                    //multi-touch event
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN,
+                        MotionEvent.ACTION_MOVE,
+                        -> {
+                            // Disallow RecyclerView to intercept touch events.
+                            itemView.parent.requestDisallowInterceptTouchEvent(true)
+                            false // Disable touch on view
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            // Allow RecyclerView to intercept touch events.
+                            itemView.parent.requestDisallowInterceptTouchEvent(false)
+                            true
+                        }
+                        else -> true
+                    }
+                } else true
+            }
+        }
 
         fun bind(galleryItem: MediaGalleryItem) {
             itemImageView.resetZoom()
