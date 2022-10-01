be74cfd694bf94363a1b914b4121855c02ff65a3
Damien Diehl <1700953+damien5314@users.noreply.github.com>
2022-10-01 08:21:15 -0400

    add activity.isDestroyed check just in case

--- app/src/main/java/com/ddiehl/android/htn/gallery/MediaGalleryRouter.kt
+++ app/src/main/java/com/ddiehl/android/htn/gallery/MediaGalleryRouter.kt
@@ -8,6 +8,11 @@ import javax.inject.Inject
 class MediaGalleryRouter @Inject constructor() {
 
     fun openLinkGallery(activity: FragmentActivity, galleryItems: List<GalleryItem>) {
+        if (activity.isDestroyed) {
+            Timber.w(IllegalStateException("Attempting to open gallery but Activity is destroyed"))
+            return
+        }
+
         Timber.d("Opening gallery with item count: %s", galleryItems.size)
         MediaGalleryFragment.create(galleryItems)
             .show(activity.supportFragmentManager, MediaGalleryFragment.TAG)
